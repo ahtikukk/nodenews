fd1b47eef4c8a44b2b596b5dd302f36e7b734eb9
Jaanus Alnek <jaanus.alnek@vykk.lan>
2019-09-30 09:21:05 +0300

    viimane 1. perioodi push

--- routes/article.js
+++ routes/article.js
@@ -2,19 +2,20 @@ const Express = require('express');
 const Router = Express.Router();
 const Article = require('./../models/articleModel');
 
-//TODO: liiguta funktsioon kontrollerisse
-Router.get('/', (req,res) => {
-    //otsime andmebaasist artiklid
-    let query = Article.find(null,null,(err, articles) =>{
-        //console.log(articles);
-        //genereerime html dokumendi vahevara abil
-        res.render('index.ejs',{
-            title: 'Latest news',
-            data: articles
+Router.get('/create', async (req,res)=>{
+    console.log(req.body.title);
+        let newArticle = new Article({
+            title : 'dfsfsdfsdf',
+            content : 'sdfsdfsdfsd',
+            description: 'aasdsgetrewterytretertertert',
+            author : '5d8de60251327120d0052622'
         });
-    });
-    
+        let result = await newArticle.save();
+        res.send(result);
 });
+
+
+
 
 Router.get('/:id',(req,res) => {
     let query = Article.find({_id : req.params.id},null,(err, articles) =>{
@@ -27,16 +28,18 @@ Router.get('/:id',(req,res) => {
     });
 });
 
-Router.post('/create', async (req,res)=>{
-    console.log(req.body.title);
-        let newArticle = new Article({
-            title : req.body.title,
-            content : req.body.content,
-            description: req.body.description,
-            author : '5d8de60251327120d0052622'
+//TODO: liiguta funktsioon kontrollerisse
+Router.get('/', (req,res) => {
+    //otsime andmebaasist artiklid
+    let query = Article.find(null,null,(err, articles) =>{
+        //console.log(articles);
+        //genereerime html dokumendi vahevara abil
+        res.render('index.ejs',{
+            title: 'Latest news',
+            data: articles
         });
-        let result = await newArticle.save();
-        res.send(result);
+    });
+    
 });
 
 module.exports = Router;
